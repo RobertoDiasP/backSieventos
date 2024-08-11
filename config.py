8c74ec73be40fce1a0cc5a4b1776c460e78f09e1
JoseDiasp <113144397+JoseDiasp@users.noreply.github.com>
2024-08-11 16:18:50 -0300

    heroku

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'aaa72b1f95adaae64a9915a4ae8ab12ca767a2935bb4620e39a91d18ae9d629f')  # Chave secreta para codificar os tokens
 
     # Configurações para Firebird
-    FIREBIRD_HOST = '200.150.199.233'  # Endereço IP do servidor Firebird
+    FIREBIRD_HOST = ''  # Endereço IP do servidor Firebird
     FIREBIRD_PORT = 3050  # Porta padrão do Firebird
-    FIREBIRD_DATABASE = '/opt/firebird/data/JNFORMATURAS.FDB'  # Caminho do banco de dados
+    FIREBIRD_DATABASE = ''  # Caminho do banco de dados
     FIREBIRD_USER = 'SYSDBA'
-    FIREBIRD_PASSWORD = 'F5sistemas'
+    FIREBIRD_PASSWORD = ''
     FIREBIRD_CHARSET = 'ISO8859_1'
